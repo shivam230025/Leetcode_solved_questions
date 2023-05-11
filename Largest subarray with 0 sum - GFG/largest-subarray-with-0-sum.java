d5dae016e0906ab523681af964dbba804d3556a4
shivam230025 <88340097+shivam230025@users.noreply.github.com>
2023-05-11 16:03:58 +0530

    Update largest-subarray-with-0-sum.java
    edge case

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
@@ -35,7 +35,7 @@ class GfG
         HashMap<Integer, Integer> map =new HashMap<>();
         int maxCount= 0;
         int sum=0;
-        map.put(sum, -1);
+        map.put(sum, -1);   // if first element is also part of the sequence then to counter that we use imaginary element 0 at index -1 to counter test case  [-1, 1, -1, 1]
         for(int i=0; i<n; i++){
             sum+= arr[i];
             if(map.containsKey(sum)){
