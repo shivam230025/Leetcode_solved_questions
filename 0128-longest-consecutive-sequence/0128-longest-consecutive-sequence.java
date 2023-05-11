fb4a333ce943d670bf45739c1ee78fea72980d42
shivam230025 <88340097+shivam230025@users.noreply.github.com>
2023-05-11 10:04:53 +0530

    Time: 26 ms (67.25%), Space: 61.2 MB (16.36%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,18 +1,46 @@
+import java.util.*;
 class Solution {
     public int longestConsecutive(int[] nums) {
         
+                                       // solution  time-O(NlogN)   space-O(1)
+        
+        // if(nums.length==0 || nums.length==1)return nums.length;
+        // Arrays.sort(nums);
+        // int count=1;
+        // int maxCount=1;
+        // for(int i=0; i<nums.length-1; i++){
+        //     if(nums[i]==nums[i+1]-1 || nums[i]==nums[i+1]){
+        //         if(nums[i]==nums[i+1]-1)count++;
+        //         maxCount= Math.max(maxCount, count);
+        //     }
+        //     else count=1;
+        // }
+        // return maxCount;
+        
+                                         
+                                      // better solution
         if(nums.length==0 || nums.length==1)return nums.length;
-        Arrays.sort(nums);
-        int count=1;
+        HashSet<Integer> set= new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+        Iterator<Integer> itr= set.iterator();
         int maxCount=1;
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i]==nums[i+1]-1 || nums[i]==nums[i+1]){
-                if(nums[i]==nums[i+1]-1)count++;
+        
+        while(itr.hasNext()){
+            
+            int num= itr.next();
+            if(set.contains(num-1)){
+                continue;
+            }else{
+                int count=1;
+                while(set.contains(num+1)){
+                    count++;
+                    num++;
+                }
                 maxCount= Math.max(maxCount, count);
             }
-            else count=1;
         }
         return maxCount;
-        
     }
 }
